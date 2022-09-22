987d36b06c047f4d3b0e2834464d5378d7e1670c
Aquamixi <debastianierick@gmail.com>
2022-09-22 07:24:51 -0300

    ajsutes rota

--- calendario_milenar/routes/web.php
+++ calendario_milenar/routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\CalendarioController::class, 'calendario_milenar']);
 
 Route::get('calendario_milenar', [App\Http\Controllers\CalendarioController::class, 'calendario_milenar']);
